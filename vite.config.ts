c12419ae568cefa4edd05316ce73c2aba7c63cfd
Geunha-Yi <75812272+Geunha-Yi@users.noreply.github.com>
2025-11-07 22:07:57 +0900

    Rollback Renaming Repository

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/2025_BP_02-01_ChuseokStatic/',
+  base: '/vue-static-page/',
 })
